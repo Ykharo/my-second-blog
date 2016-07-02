34cfa77a7e19574ec1ef5ee86f8fc4bd67f1282c
Ykharo <micahelleyton@hotmail.com>
2016-07-02 12:15:42 -0300

    Mi app2 Django Girls,primer commit

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    author =models.ForeignKey('auth.User')
+    title = models.CharField(max_length =200)
+    text = models.TextField()
+    create_date = models.DateTimeField( default=timezone.now())
+    pubished_date = models.DateField(blank=True,null=True)
+    
+    def publish(self):
+        self.pubished_date = timezone.now()
+        self.save()
+        
+    def __srt__(self):
+        return self.title
